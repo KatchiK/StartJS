67ab8c2e9f1a8410b01887114fcbbc7fc25a655d
KatchiK <a_kachalov@mail.ru>
2021-04-02 17:56:48 +0300

    Назначил переменные, вывел инф. в мод. окно, вывел текст в консоль

--- script/script.js
+++ script/script.js
@@ -1,7 +1,18 @@
 
 
-// console.log(3);
-// alert(1);
-// console.log(2);
 
-var myVar = 10;
+// назначение переменных
+  let money = 'деньги';
+  let income = 'Вывести на экран в модальном окне (alert) сообщение с любым текстом';
+  let addExpenses = 'Расход';
+  let deposit = '1000000';
+  let mission = 'Вывести в консоль сообщение с любым текстом';
+  let period = 'Период';
+
+  // Вывод в модальное окно
+  alert(income);
+
+  //Вывод в консоль
+  console.log(mission);
+
+
