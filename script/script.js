0d09cf5be85af0b54fec0f3f807acab6eed12ce4
KatchiK <a_kachalov@mail.ru>
2021-04-05 00:22:17 +0300

    assigned data to variables; output to the console

--- script/script.js
+++ script/script.js
@@ -2,17 +2,28 @@
 
 
 // объявление переменных
-  let money = 534635;
+  let money = 990000;
   let income = 'строка с дополнительными доходом (например: фриланс)';
-  let addExpenses = 'интернет, такси, коммуналка';
+  let addExpenses = 'Интернет, Такси, Коммуналка';
   let deposit = true;
   let mission = 5000000000000000000;
   let period = 9;
 
+
   // Вывод в модальное окно
-  alert(income);
+  // alert(income);
 
   //Вывод в консоль
-  console.log(mission);
+  console.log(typeof money); // вывод типа данных
+  console.log(typeof income); // вывод типа данных
+  console.log(typeof deposit); // вывод типа данных
+  console.log(addExpenses.length); // вывод количества символов
+  console.log('Период равен ' + period + ' месяцев'); //Период равен (period) месяцев
+  console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');//Цель заработать
+  console.log(addExpenses.toLowerCase().split(', '));//к нижнему регистру и вывод массива
 
+//объявление переменных
+  let budgetDay = (money/30);
 
+  //Вывод в консоль
+  console.log(budgetDay);//вывод дневного бюджета
